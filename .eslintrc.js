70f01808285ccef35c4246951c0454741391c0cf
uyriq <uyriq@local>
2022-08-30 22:13:28 +0300

    официальный пример c сайта Redux Toolkit обновил react 18.2 , @reduxjs/toolkit 1.8.5 , redux 8.0.2

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
+    },
+
+    extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', 'prettier'],
+    rules: {
+        'linebreak-style': 0,
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto',
+                tabWidth: 4,
+            },
+        ],
+        // Indent with 4 spaces
+        indent: ['off', 4],
+
+        // Indent JSX with 4 spaces
+        'react/jsx-indent': ['off', 4],
+
+        // Indent props with 4 spaces
+        'react/jsx-indent-props': ['off', 4],
+    },
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+/* eslint-disable indent */
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import './index.css'
+import App from './App'
+import store from './app/store'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
+
+root.render(
+    // Оборачиваем приложение компонентом Provider из пакета react-redux
+    // eslint-disable-next-line react/jsx-filename-extension
+    <StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </StrictMode>
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
